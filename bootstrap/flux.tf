1eb715d98746c34f359c5f4af61f1620ef196076
Denys Vasyliev <den.vasyliev@gmail.com>
2025-05-20 21:15:34 +0000

    add demokit

--- bootstrap/flux.tf
+++ bootstrap/flux.tf
@@ -3,7 +3,6 @@
 # ==========================================
 resource "helm_release" "flux_operator" {
   depends_on = [kind_cluster.this]
-
   name             = "flux-operator"
   namespace        = "flux-system"
   repository       = "oci://ghcr.io/controlplaneio-fluxcd/charts"
@@ -15,13 +14,12 @@ resource "helm_release" "flux_operator" {
 # ==========================================
 resource "helm_release" "flux_instance" {
   depends_on = [helm_release.flux_operator]
-
   name       = "flux-instance"
   namespace  = "flux-system"
   repository = "oci://ghcr.io/controlplaneio-fluxcd/charts"
   chart      = "flux-instance"
   set {
     name  = "distribution.version"
-    value = "=2.5.x"
+    value = "=2.x.x"
   }
 }
--- bootstrap/gatewayapi.tf
+++ bootstrap/gatewayapi.tf
@@ -3,6 +3,7 @@
 # ==========================================
 resource "helm_release" "envoy_gateway" {
   depends_on       = [kind_cluster.this]
+
   name             = "eg"
   namespace        = "envoy-gateway-system"
   repository       = "oci://docker.io/envoyproxy"
